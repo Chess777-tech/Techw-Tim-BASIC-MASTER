b871b59e287e21065d471908117fcb7cd84c02eb
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-12-11 13:08:49 -0600

    Add files via upload

---
+++ iteration_by_item.py
@@ -0,0 +1,25 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> fruits = ['apples','pear','strawberrys']
+>>> for fruit in fruits:
+...     print(fruit)
+... 
+...     
+apples
+pear
+strawberrys
+>>> 
+>>> 
+>>> fruits = ['apples','pears','strawberries']
+>>> for fruit in fruits:
+...     if fruit == 'pears':
+...         print('Item Sold Out...')
+...     else:
+...         print('Adding Item...')
+... 
+...         
+Adding Item...
+Item Sold Out...
+Adding Item...
+>>> 
+>>> 
