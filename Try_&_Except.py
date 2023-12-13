a6cab4e71a1a22eb209792fc341bb2def27695ef
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-12-13 11:46:01 -0600

    Try_&_Except.py

--- Try_&_Except.py
+++ Try_&_Except.py
@@ -1,7 +1,6 @@
 Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> text = input('Username: ')
-Username: Colby
 >>> try:
 ...     number = int(text)
 ...     print(number)
@@ -9,5 +8,4 @@ Username: Colby
 ...     print('Invalid Username... ')
 ... 
 ...     
-Invalid Username... 
 >>> 
