3d258fc883f36a9e27a205ef35fbf970b4d237ac
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-12-13 11:45:36 -0600

    Add files via upload

---
+++ Try_&_Except.py
@@ -0,0 +1,13 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> text = input('Username: ')
+Username: Colby
+>>> try:
+...     number = int(text)
+...     print(number)
+... except:
+...     print('Invalid Username... ')
+... 
+...     
+Invalid Username... 
+>>> 
