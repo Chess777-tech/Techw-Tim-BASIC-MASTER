7cde07f234100c60b40ee00435e59fc006e83425
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-12-16 12:48:08 -0600

    Add files via upload

---
+++ static_class_methods.py
@@ -0,0 +1,24 @@
+#static method 
+
+class myClass:
+    def __init__(self):
+        self.x =x 
+    
+    @staticmethod
+    def staticmethod():
+        return "I am a static method"
+
+# Notice staticmethod does not requre the self parameter 
+    
+
+
+#class method 
+
+class myClass:
+    def __init__(self):
+        self.x = x 
+    
+    @classmethod
+    def classmethod(cls):
+        cls.count += 1
+# The classMethod can access and modify class variables. It takes the class name as a required parameter 
