e938fc98c89303ea542da6f3caddb2dd7f2ec149
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-12-18 09:46:33 -0600

    Add files via upload

---
+++ optional_parameters.py
@@ -0,0 +1,22 @@
+def my_func(x=5):
+    print(x)
+
+my_func()
+my_func(10)
+
+
+def myFunc(x=5, y = 6 ):
+    print(x+y)
+
+
+myFunc()      # Prints 11
+myFunc(12)    # Prints 18
+myFunc(10,10) # Prints 20
+
+
+def my_Func(x, y=6):
+    print(x+y)
+
+
+my_Func(12)
+my_Func(10,10)
